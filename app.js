c597261e3e27043cb86b158e3cffd9e30ba68632
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-04-27 20:53:58 +0530

    db conn & login

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const connectDB = require('./db')
+const mongoose = require('mongoose')
+const body = require('body-parser')
+const app = express()
+const { MongoClient } = require('mongodb')
+
+const handlebars = require("express-handlebars");
+app.set("view engine", "handlebars");
+app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.get('/login',(req,res) => {
+  res.render('login.hbs')
+})
+
+app.listen(3000, async () => {
+    await connectDB();
+    console.log(`http://localhost:3000`);
+  });
---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+const connectDB = async ()=> {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+      // dbConnection = conn.connection.db
+      // console.log(dbConnection)
+    } catch (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  };
+  
+  module.exports = connectDB
