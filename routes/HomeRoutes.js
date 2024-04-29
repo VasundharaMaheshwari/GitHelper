61602d8f0759238497afebf5f6d25a8632cda8a2
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-04-29 12:03:18 +0530

    minor

--- routes/HomeRoutes.js
+++ routes/HomeRoutes.js
@@ -4,4 +4,8 @@ const { refresh } = require('../controllers/HomeController')
 
 HomeRouter.get('/',refresh)
 
+HomeRouter.get('/view',(req,res) => {
+    return res.send('Success')
+})
+
 module.exports = HomeRouter
