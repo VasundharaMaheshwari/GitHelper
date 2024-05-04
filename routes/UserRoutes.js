f1b1ceff4a4ade5e87543fa16b76e629dff99a2b
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-05-04 19:17:42 +0530

    minor

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -8,11 +8,11 @@ UserRouter.post('/login',login)
 UserRouter.post('/register',register)
 
 UserRouter.get('/login',(req,res) => {
-    res.render('login.hbs')
+    return res.render('login.hbs')
 })
 
 UserRouter.get('/register',(req,res) => {
-    res.render('register.hbs')
+    return res.render('register.hbs')
 })
 
 UserRouter.get('/user',restrict,load)
