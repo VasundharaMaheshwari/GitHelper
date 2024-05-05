334f4bfd385dcc6a55fc22c5f10d7f4b4b0d44f1
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-05-06 01:58:42 +0530

    minor

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -74,6 +74,9 @@ const query_check = async (req,res,next) => {
     const {queryId} = req.query
     if(ObjectId.isValid(queryId) && await Issue.findOne({"_id": queryId})){
     req.user = user
+    if(user.role == "Admin"){
+        return res.status(403).redirect('/admin')
+    }
     return next()
     }
     return res.status(403).redirect('/query/list')
