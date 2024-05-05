519d0eda5ec5b4adac7d57f555f2e28202733366
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-05-05 19:35:31 +0530

    minor

--- middlewares/rate_limiter.js
+++ middlewares/rate_limiter.js
@@ -19,18 +19,8 @@ const login_limit = limiter({
         const date = new Date(req.rateLimit.resetTime)
         req.rateLimit.resetTime = date.toLocaleTimeString()
         return res.redirect(`/error?error_details=Login_Disabled_Due_To_Too_Many_Failed_Attempts_Till_${req.rateLimit.resetTime}`)
-    }
+    },
+    skipSuccessfulRequests: true
 })
 
-const refresh_limit = limiter({
-    windowMs: 60*1000,
-    max: 5,
-    legacyHeaders: false,
-    handler: (req,res) => {
-        const date = new Date(req.rateLimit.resetTime)
-        req.rateLimit.resetTime = date.toLocaleTimeString()
-        return res.redirect(`/error?error_details=Too_Many_Requests_Received_Please_Wait_Till_${req.rateLimit.resetTime}`)
-    }
-})
-
-module.exports = { register_limit,login_limit,refresh_limit }
+module.exports = { register_limit,login_limit }
