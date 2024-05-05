129b6eff45834d2f5a0e21405bdbab624810754e
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-05-05 20:56:39 +0530

    rate limit fixed

--- middlewares/rate_limiter.js
+++ middlewares/rate_limiter.js
@@ -4,6 +4,7 @@ const register_limit = limiter({
     windowMs: 24*60*60*1000,
     max: 3,
     legacyHeaders: false,
+    requestWasSuccessful: (req, res) => res.status < 400,
     skipFailedRequests: true,
     handler: (req,res) => {
         const date = new Date(req.rateLimit.resetTime)
@@ -16,18 +17,21 @@ const login_limit = limiter({
     windowMs: 60*60*1000,
     max: 5,
     legacyHeaders: false,
+    requestWasSuccessful: (req, res) => res.status < 400,
     skipSuccessfulRequests: true,
     handler: (req,res) => {
         const date = new Date(req.rateLimit.resetTime)
         req.rateLimit.resetTime = date.toLocaleTimeString()
         return res.status(429).redirect(`/error?error_details=Login_Disabled_Due_To_Too_Many_Attempts_Till_${req.rateLimit.resetTime}`)
-    }
+    },
+    keyGenerator: (req,res) => req.body.username
 })
 
 const issue_limit = limiter({
     windowMs: 24*60*60*1000,
     max: 5,
     legacyHeaders: false,
+    requestWasSuccessful: (req, res) => res.status < 400,
     skipFailedRequests: true,
     handler: (req,res) => {
         const date = new Date(req.rateLimit.resetTime)
@@ -41,6 +45,7 @@ const response_limit = limiter({
     windowMs: 10*60*60*1000,
     max: 10,
     legacyHeaders: false,
+    requestWasSuccessful: (req, res) => res.status < 400,
     skipFailedRequests: true,
     handler: (req,res) => {
         const date = new Date(req.rateLimit.resetTime)
