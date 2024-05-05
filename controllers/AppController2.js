7762944a740a024f72b5f68109fd4cbc3e16acf6
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-05-06 00:42:55 +0530

    user edit

--- controllers/AppController2.js
+++ controllers/AppController2.js
@@ -47,4 +47,34 @@ const show_res = (req,res) => {
     return res.status(201).send('Success')
 }
 
-module.exports = { edit,delete_query,show_res }
+const save_edit = async (req,res) => {
+  try{
+    const {_id} = req.query
+    if(ObjectId.isValid(_id)){
+      const second = await Issue.findOne({"repo_link": req.body.repo_link})
+      if(_id == second._id){
+        const first = await Issue.findOneAndUpdate({"_id": _id},{
+          username: req.user.username,
+          contact_info: req.body.contact_info,
+          skillset: req.body.skillset,
+          github_id: req.body.github_id,
+          repo_link: req.body.repo_link,
+          description: req.body.description
+        })
+        if(first){
+          return res.status(200).redirect('/query/list')
+        } else {
+          return res.status(403).redirect('/error?error_details=Unable_To_Delete_Query')
+        }
+      }else {
+        return res.status(403).redirect('/error?error_details=Already_Exists')
+      }
+    } else {
+      return res.status(404).redirect('/error?error_details=Invalid_URL')
+    }
+  } catch(err) {
+    return res.status(500).redirect('/error?error_details=Error_Occurred')
+  }
+}
+
+module.exports = { edit,delete_query,show_res,save_edit }
--- routes/AppRoutes.js
+++ routes/AppRoutes.js
@@ -1,7 +1,7 @@
 const express =  require('express')
 const APIRouter = express.Router()
 const { create,save,list,responder,save_response } = require('../controllers/AppController')
-const { edit,delete_query,show_res } = require('../controllers/AppController2')
+const { edit,delete_query,show_res,save_edit } = require('../controllers/AppController2')
 const { issue_limit,response_limit } = require('../middlewares/rate_limiter')
 
 APIRouter.get('/create',create)
@@ -16,9 +16,7 @@ APIRouter.post('/respond',response_limit,save_response)
 
 APIRouter.get('/edit',edit)
 
-APIRouter.post('/edit_query',(req,res) => {
-    return res.status(200).send('WIP')
-})
+APIRouter.post('/edit_query',save_edit)
 
 APIRouter.get('/delete',delete_query)
 
