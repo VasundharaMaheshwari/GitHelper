a8be7b9d2f8f152d4eda982686f2503901862b9d
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-08-29 23:22:13 +0530

    error side validation working

--- validators/ErrorValidators.js
+++ validators/ErrorValidators.js
@@ -4,7 +4,6 @@ const ErrorCheck = query('error_details')
   .trim()
   .notEmpty()
   .matches(/^[A-Za-z_]+$/)
-  .escape()
   .withMessage('Invalid Input: Only alphabets and underscores are allowed.');
 
 module.exports = { ErrorCheck };
