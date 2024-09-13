52e520439f4e9f865e5b59364e5de9574de8e763
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-09-14 03:09:50 +0530

    bug fix

--- controllers/ChatController.js
+++ controllers/ChatController.js
@@ -5,10 +5,10 @@ const chatload = async (req,res) => {
     try{
         const checker2 = ObjectId.isValid(req.user._id)
         if(checker2){
-        const checker = await Response.findOne({"responder":{"username": req.query.username},"creator": req.user._id})
+        const checker = await Response.findOne({"responder.username": req.query.username,"creator": req.user._id})
         if(checker) {
     return res.status(200).render('main.hbs',{layout: "chat.hbs",
-        receiverUsername: req.query.receiverUsername
+        receiverUsername: req.query.username
       }
     )
 } else {
