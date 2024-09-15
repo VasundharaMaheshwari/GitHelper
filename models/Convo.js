2fe55c9e041c06616eb58bbb7d1030d76216e237
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-09-16 00:31:39 +0530

    schemas chat

---
+++ models/Convo.js
@@ -0,0 +1,24 @@
+const { Schema, default: mongoose } = require('mongoose')
+
+const Convo_Schema = new Schema({
+    "issue": {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Issue'
+    },
+    "initiator": {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'GHUser'
+    },
+    "receiver": {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'GHUser'
+    }
+},
+{timestamps: true})
+
+const Convo = mongoose.model('Convo',Convo_Schema)
+
+module.exports = { Convo }
---
+++ models/Msg.js
@@ -0,0 +1,28 @@
+const { Schema, default: mongoose } = require('mongoose')
+
+const Msg_Schema = new Schema({
+    "convoId": {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Convo'
+    },
+    "receiver": {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'GHUser'
+    },
+    "msg": {
+        type: String,
+        required: true
+    },
+    "sender": {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'GHUser'
+    }
+},
+{timestamps:true})
+
+const Msg = mongoose.model('Msg',Msg_Schema)
+
+module.exports = { Msg }
