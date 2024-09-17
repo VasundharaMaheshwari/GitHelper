6f97e2ac996e0f3257e3e8c3b2db8ba0b4fa354e
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-09-17 17:08:25 +0530

    notes

--- routes/ChatRoutes.js
+++ routes/ChatRoutes.js
@@ -12,7 +12,7 @@ ChatRouter.get('/list',chatlist)
 ChatRouter.get('/chats',checkchat,chatting)
 
 ChatRouter.post('/chats',(req,res) => {
-    return res.status(200).send("Developing... Need to delete convos and messages when deleting issues now... Implement middleware to see if first approval or not then redirect to chats... See ongoing responses and closing responses == delete convo and messages from either side and vice versa if initiator... Pre load history etc.")
+    return res.status(200).send("Developing... Update deleting convos and messages when deleting issues now... Implement middleware to see if first approval or not then redirect to chats... See ongoing responses and closing responses == delete convo and messages from either side and vice versa if initiator... Pre load history etc.")
 })
 
 module.exports = ChatRouter
