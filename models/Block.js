525ddc1fec0f675ea960fdcafb6eb6797159e6fc
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-09-26 20:14:52 +0530

    added blocklist email schema

---
+++ models/Block.js
@@ -0,0 +1,13 @@
+const { Schema, default: mongoose } = require('mongoose')
+
+const Block_Schema = new Schema({
+    "email": {
+        type: String,
+        required: true
+    }
+},
+{timestamps:true})
+
+const Block = mongoose.model('Block',Block_Schema)
+
+module.exports = { Block }
