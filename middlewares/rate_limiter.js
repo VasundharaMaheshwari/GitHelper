5b181275e7282c9828eb1cce20f83f5287fd677a
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-10-01 21:36:50 +0530

    trial

--- middlewares/rate_limiter.js
+++ middlewares/rate_limiter.js
@@ -11,7 +11,13 @@ const register_limit = limiter({
         req.rateLimit.resetTime = date.toLocaleString()
         return res.status(429).redirect(`/error?error_details=Cannot_Create_More_Accounts_Till_${req.rateLimit.resetTime}`)
     },
-    keyGenerator: (req,res) => req.ip
+    keyGenerator: (req) => {
+        const xForwardedFor = req.headers['x-forwarded-for'];
+        if (xForwardedFor) {
+          return xForwardedFor.split(',')[0].trim();
+        }
+        return req.ip;
+      }
 })
 
 const login_limit = limiter({
@@ -39,7 +45,13 @@ const login_limit_ip = limiter({
         req.rateLimit.resetTime = date.toLocaleString()
         return res.status(429).redirect(`/error?error_details=Login_Completely_Disabled_Till_${req.rateLimit.resetTime}`)
     },
-    keyGenerator: (req,res) => req.ip
+    keyGenerator: (req) => {
+        const xForwardedFor = req.headers['x-forwarded-for'];
+        if (xForwardedFor) {
+          return xForwardedFor.split(',')[0].trim();
+        }
+        return req.ip;
+      }
 })
 
 const issue_limit = limiter({
@@ -107,7 +119,13 @@ const overall_limit = limiter({
     handler: (req,res) => {
         return res.status(429).send("Not Allowed")
     },
-    keyGenerator: (req,res) => req.ip
+    keyGenerator: (req) => {
+        const xForwardedFor = req.headers['x-forwarded-for'];
+        if (xForwardedFor) {
+          return xForwardedFor.split(',')[0].trim();
+        }
+        return req.ip;
+      }
 })
 
 module.exports = { register_limit,login_limit,issue_limit,response_limit,login_limit_ip,edit_limit,convo_limit,overall_limit }
