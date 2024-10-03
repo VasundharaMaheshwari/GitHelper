a386c536c4ef5866d6360946134d14e390f88712
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-10-03 20:38:43 +0530

    notes

--- routes/ChatRoutes.js
+++ routes/ChatRoutes.js
@@ -13,7 +13,7 @@ ChatRouter.get('/list',chatlist)
 ChatRouter.get('/chats',checkchat,chat_check,chatting)
 
 ChatRouter.post('/chats',(req,res) => {
-    return res.status(200).send("Auto-scroll down after reload chat history... Forgot password... Response deleted Register Inbuilt... Chat Input Santiation + Rate Limit... Task Tracking... See ongoing responses and closing responses == delete convo and messages from either side and vice versa if initiator...")
+    return res.status(200).send("Forgot password... Chat Input Santiation + Rate Limit... Task Tracking... See ongoing responses and closing responses == delete convo and messages from either side and vice versa if initiator...")
 })
 
 module.exports = ChatRouter
