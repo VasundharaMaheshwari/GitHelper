791eec4b8874f7df9574dd75a8f7200d50c4a33a
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-10-03 20:34:11 +0530

    minor

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -92,7 +92,7 @@ const save_response = async (req,res) => {
       approved: false
     })
     await response_.save()
-    return res.status(201).redirect('/home')} else {
+    return res.status(201).redirect('/home#queries')} else {
       return res.status(403).redirect('/error?error_details=Already_Responded')
     }
   }else{
