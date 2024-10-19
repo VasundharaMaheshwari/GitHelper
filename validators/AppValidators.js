7da0c4a441462c599872caaf08abf388d3aa87c2
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-10-19 23:59:50 +0530

    delete fix + own repo check

--- validators/AppValidators.js
+++ validators/AppValidators.js
@@ -2,8 +2,15 @@ const { body,query } = require('express-validator')
 
 const saveIssue = [body('contact_info').trim().notEmpty().matches(/^[0-9]{10}$/),
     body('skillset').trim().notEmpty().matches(/^[a-zA-Z0-9,.\s]+$/),
-    // body('github_id').trim().notEmpty().matches(/^[a-zA-Z0-9-]{1,39}$/),
-    body('repo_link').trim().notEmpty().matches(/^(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9-]+\/[a-zA-Z0-9-_]+(\/)?$/),
+    body('github_id').trim().notEmpty().matches(/^[a-zA-Z0-9-]{1,39}$/),
+    body('repo_link').trim().notEmpty().custom((value, { req }) => {
+      const githubId = req.body.github_id;
+      const repoRegex = new RegExp(`^(https?:\\/\\/)?(www\\.)?github\\.com\\/${githubId}\\/([a-zA-Z0-9-_]+)(\\/)?$`);
+      if (!repoRegex.test(value)) {
+        throw new Error('Invalid GitHub repository link.');
+      }
+      return true; 
+    }),
     body('description').trim().notEmpty().matches(/^[a-zA-Z0-9.,!?;:\s]+$/)
 ]
 
