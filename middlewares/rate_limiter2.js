16544d78bfc9b541a2247fa37e47fc91b3cfb8d9
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-12-10 15:19:41 +0530

    updates

---
+++ middlewares/rate_limiter2.js
@@ -0,0 +1,23 @@
+const limiter = require('express-rate-limit');
+
+const forget_limit_ip = limiter({
+  windowMs: 24 * 60 * 60 * 1000,
+  max: 15,
+  legacyHeaders: false,
+  // requestWasSuccessful: (req, res) => res.status < 400,
+  // skipSuccessfulRequests: true,
+  handler: (req, res) => {
+    const date = new Date(req.rateLimit.resetTime);
+    req.rateLimit.resetTime = date.toLocaleString();
+    return res.status(429).redirect(`/error?error_details=Too_Many_Requests_To_Reset_Password_Please_Wait_Till_${req.rateLimit.resetTime}`);
+  },
+  keyGenerator: (req) => {
+    const xForwardedFor = req.headers['x-forwarded-for'];
+    if (xForwardedFor) {
+      return xForwardedFor.split(',')[0].trim();
+    }
+    return req.ip;
+  }
+});
+
+module.exports = { forget_limit_ip };
