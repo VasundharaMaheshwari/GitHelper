44102f096cdec3a08de3c61eaaa1cb36d2d1df04
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2025-03-09 01:33:35 +0530

    minor

--- validators/ChatValidators.js
+++ validators/ChatValidators.js
@@ -1,7 +1,6 @@
 const { query, body } = require('express-validator');
 
-const checkchat = [query('username').trim().notEmpty().matches(/^[a-zA-Z0-9_]+$/)
-];
+const checkchat = [query('username').trim().notEmpty().matches(/^[a-zA-Z0-9_]+$/)];
 
 const checkchat2 = [body('username').trim().notEmpty().matches(/^[a-zA-Z0-9_]+$/),
   body('response').trim().notEmpty().matches(/^[a-fA-F0-9]{24}$/)
