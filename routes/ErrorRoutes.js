115d16a9a779ddfecaa448687d065303ae6f7390
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2025-03-30 19:39:26 +0530

    profile view

--- routes/ErrorRoutes.js
+++ routes/ErrorRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const ErrorRouter = express.Router();
-const { error, contactus, leaderboard } = require('../controllers/ErrorController');
+const { error, contactus, leaderboard, profileLoad } = require('../controllers/ErrorController');
 const { ErrorCheck, msgCheck } = require('../validators/ErrorValidators');
 const { contact_limit } = require('../middlewares/rate_limiter');
 
@@ -33,5 +33,7 @@ ErrorRouter.get('/contact', (req, res) => {
 ErrorRouter.get('/leaderboard', leaderboard);
 
 ErrorRouter.post('/send', msgCheck, contact_limit, contactus);
+
+ErrorRouter.get('/profile/:username', profileLoad);
 
 module.exports = ErrorRouter;
