e3dafa0c1a71bfa7e5ac3c1cd1993e4d68093304
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2025-04-03 11:10:35 +0530

    closing works

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -2,6 +2,8 @@ const { loader, deleter, userlist, usermod, viewer, detailsBan, bannedUsers } = 
 const express = require('express');
 const { idcheck } = require('../validators/AdminValidators');
 const { Report } = require('../models/Report');
+const { ObjectId } = require('mongodb');
+const { default: mongoose } = require('mongoose');
 
 const AdminRouter = express.Router();
 
@@ -21,7 +23,7 @@ AdminRouter.get('/view', idcheck, viewer);
 
 AdminRouter.get('/reported', async (req, res) => {
   try {
-    const reports = await Report.find()
+    const reports = await Report.find({ closed_by: null })
       .sort({ createdAt: -1 })
       .populate('createdBy', 'username')
       .populate('createdAgainst', 'username')
@@ -36,5 +38,27 @@ AdminRouter.get('/reported', async (req, res) => {
   }
 });
 
+AdminRouter.post('/closeReport', async (req, res) => {
+  try {
+    let { reportId } = req.body;
+
+    reportId = new mongoose.Types.ObjectId(reportId);
+
+    if (!ObjectId.isValid(reportId)) return res.status(400).redirect('/error?error_details=Invalid');
+
+    const available = await Report.findById(reportId);
+    if (!available) return res.status(400).redirect('/error?error_details=Report_Not_Found');
+
+    if (req.user.role === 'Admin') {
+      await Report.findByIdAndUpdate(reportId, { closed_by: req.user._id });
+    } else {
+      return res.status(401).redirect('/error?error_details=Only_Admins_Can_Close_Queries');
+    }
+
+    return res.status(201).redirect('/admin/reported');
+  } catch {
+    return res.status(500).send('Internal Server Error');
+  }
+});
 
 module.exports = AdminRouter;
