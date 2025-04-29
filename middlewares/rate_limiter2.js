997e44ba1e6b0d16f5265143eca3bd33f3de077d
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2025-04-29 19:54:11 +0530

    blockchain limiters done

--- middlewares/rate_limiter2.js
+++ middlewares/rate_limiter2.js
@@ -120,4 +120,64 @@ const updater_ip = limiter({
   }
 });
 
-module.exports = { forget_limit_ip, verify_limit_ip, close_report_ip, delete_admin_ip, gen_url_ip, updater_ip };
+const wallet_ip = limiter({
+  windowMs: 24 * 60 * 60 * 1000,
+  max: 2,
+  legacyHeaders: false,
+  // requestWasSuccessful: (req, res) => res.status < 400,
+  // skipSuccessfulRequests: true,
+  handler: (req, res) => {
+    const date = new Date(req.rateLimit.resetTime);
+    req.rateLimit.resetTime = date.toLocaleString();
+    return res.status(429).redirect(`/error?error_details=Please_Wait_Till_${req.rateLimit.resetTime}_To_Connect_Your_Wallet`);
+  },
+  keyGenerator: (req) => {
+    const xForwardedFor = req.headers['x-forwarded-for'];
+    if (xForwardedFor) {
+      return xForwardedFor.split(',')[0].trim();
+    }
+    return req.ip;
+  }
+});
+
+const claim_ip = limiter({
+  windowMs: 24 * 60 * 60 * 1000,
+  max: 2,
+  legacyHeaders: false,
+  // requestWasSuccessful: (req, res) => res.status < 400,
+  // skipSuccessfulRequests: true,
+  handler: (req, res) => {
+    const date = new Date(req.rateLimit.resetTime);
+    req.rateLimit.resetTime = date.toLocaleString();
+    return res.status(429).redirect(`/error?error_details=Please_Wait_Till_${req.rateLimit.resetTime}_To_Claim_Your_Points`);
+  },
+  keyGenerator: (req) => {
+    const xForwardedFor = req.headers['x-forwarded-for'];
+    if (xForwardedFor) {
+      return xForwardedFor.split(',')[0].trim();
+    }
+    return req.ip;
+  }
+});
+
+const redeem_ip = limiter({
+  windowMs: 24 * 60 * 60 * 1000,
+  max: 15,
+  legacyHeaders: false,
+  // requestWasSuccessful: (req, res) => res.status < 400,
+  // skipSuccessfulRequests: true,
+  handler: (req, res) => {
+    const date = new Date(req.rateLimit.resetTime);
+    req.rateLimit.resetTime = date.toLocaleString();
+    return res.status(429).redirect(`/error?error_details=Please_Wait_Till_${req.rateLimit.resetTime}_To_Redeem_Your_Tokens`);
+  },
+  keyGenerator: (req) => {
+    const xForwardedFor = req.headers['x-forwarded-for'];
+    if (xForwardedFor) {
+      return xForwardedFor.split(',')[0].trim();
+    }
+    return req.ip;
+  }
+});
+
+module.exports = { forget_limit_ip, verify_limit_ip, close_report_ip, delete_admin_ip, gen_url_ip, updater_ip, wallet_ip, claim_ip, redeem_ip };
--- routes/BlockChainRoutes.js
+++ routes/BlockChainRoutes.js
@@ -2,19 +2,20 @@ const express = require('express');
 const { connectWallet, displayWallet, tokenConversion, pushRequests, confirmed, rejected } = require('../controllers/BlockChainController');
 const { walletNotConnected, walletConnected } = require('../middlewares/middleware');
 const { walletCheckAddress, pointsCheck, prepTrans, rejectedCheck, acceptedCheck } = require('../validators/BlockChainValidators');
+const { wallet_ip, claim_ip, redeem_ip } = require('../middlewares/rate_limiter2');
 
 const BlockChainRouter = express.Router();
 
 BlockChainRouter.get('/wallet-display', walletConnected, displayWallet);
 
-BlockChainRouter.post('/save-wallet', walletCheckAddress, walletNotConnected, connectWallet);
+BlockChainRouter.post('/save-wallet', walletCheckAddress, wallet_ip, walletNotConnected, connectWallet);
 
-BlockChainRouter.post('/claim', pointsCheck, walletConnected, tokenConversion);
+BlockChainRouter.post('/claim', pointsCheck, claim_ip, walletConnected, tokenConversion);
 
-BlockChainRouter.post('/prepare-transaction', prepTrans, walletConnected, pushRequests);
+BlockChainRouter.post('/prepare-transaction', prepTrans, redeem_ip, walletConnected, pushRequests);
 
-BlockChainRouter.post('/confirm-transaction', acceptedCheck, walletConnected, confirmed);
+BlockChainRouter.post('/confirm-transaction', acceptedCheck, redeem_ip, walletConnected, confirmed);
 
-BlockChainRouter.post('/reject-transaction', rejectedCheck, walletConnected, rejected);
+BlockChainRouter.post('/reject-transaction', rejectedCheck, redeem_ip, walletConnected, rejected);
 
 module.exports = BlockChainRouter;
