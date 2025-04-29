db93ef8dc6af58bcb2a8e3b47719dfd317343e25
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2025-04-29 14:34:10 +0530

    minor

--- routes/BlockChainRoutes.js
+++ routes/BlockChainRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { connectWallet, displayWallet, tokenConversion, pushRequests, confirmed, rejected } = require('../controllers/BlockChainController');
 const { walletNotConnected, walletConnected } = require('../middlewares/middleware');
-const { walletCheckAddress, pointsCheck, prepTrans, rejectedCheck } = require('../validators/BlockChainValidators');
+const { walletCheckAddress, pointsCheck, prepTrans, rejectedCheck, acceptedCheck } = require('../validators/BlockChainValidators');
 
 const BlockChainRouter = express.Router();
 
@@ -13,7 +13,7 @@ BlockChainRouter.post('/claim', pointsCheck, walletConnected, tokenConversion);
 
 BlockChainRouter.post('/prepare-transaction', prepTrans, walletConnected, pushRequests);
 
-BlockChainRouter.post('/confirm-transaction', walletConnected, confirmed);
+BlockChainRouter.post('/confirm-transaction', acceptedCheck, walletConnected, confirmed);
 
 BlockChainRouter.post('/reject-transaction', rejectedCheck, walletConnected, rejected);
 
